1fd53db057d14196320869392ca2e54dfb552c62
jbognar <josip.bognar@optinet.hr>
2019-04-22 18:18:42 +0100

    another

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BuildingsComponent } from './buildings/buildings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'buildings', component: BuildingsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
